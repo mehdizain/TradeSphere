8b68a224420be4cb7ad93dcf567119a88f04c6c6
Mehdi Zain <mzainmehdi@yahoo.com>
2025-04-30 16:30:52 +0500

    Updated Kotlin files.

--- app/src/main/java/com/example/tradesphere/MainActivity.kt
+++ app/src/main/java/com/example/tradesphere/MainActivity.kt
@@ -1,44 +1,15 @@
 package com.example.tradesphere
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import com.google.android.material.tabs.TabLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var tabLayout: TabLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
-        tabLayout = findViewById(R.id.tabLayout)
-
-        // Set default fragment
-        replaceFragment(HomeFragment())
-
-        // Set listener for tab selection
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                val selectedFragment: Fragment = when (tab?.position) {
-                    0 -> HomeFragment()
-                    1 -> SearchFragment()
-                    2 -> ChatFragment()
-                    3 -> AccountFragment()
-                    else -> HomeFragment()
-                }
-                replaceFragment(selectedFragment)
-            }
-
-            override fun onTabUnselected(tab: TabLayout.Tab?) {}
-            override fun onTabReselected(tab: TabLayout.Tab?) {}
-        })
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.frameLayout, fragment)
-            .commit()
     }
 }
