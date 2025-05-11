50c2c00ea5df31aa7806630835430aff4748cc57
zaidali.1 <zaid09104@gmail.com>
2025-05-11 14:39:49 +0500

    updated UI and removed actionbar

--- app/src/main/java/com/example/tradesphere/MainActivity.kt
+++ app/src/main/java/com/example/tradesphere/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
 
         val viewPager: ViewPager2 = findViewById(R.id.viewPager)
